dfd0fd5019043bb2cd4b0296034b2bc4ca8ec0fc
nazar001 <nazar-m1@ulster.ac.uk>
2025-12-01 02:14:03 +0000

    started bottom navigation menu

--- app/src/main/java/com/example/courseworkproject/HomeActivity.java
+++ app/src/main/java/com/example/courseworkproject/HomeActivity.java
@@ -1,5 +1,49 @@
 package com.example.courseworkproject;
 
-public class HomeActivity {
+import android.os.Bundle;
+import android.util.Log;
+import android.view.MenuItem;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import com.example.courseworkproject.R;
+import com.example.courseworkproject.databinding.ActivityMainBinding;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
+
+public class HomeActivity extends AppCompatActivity {
+    ActivityMainBinding binding;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        replaceFragment(new HomeFragment());
+        binding.bottomNavigationView.setOnItemSelectedListener(item -> {
+            int id = item.getItemId();
+            if (id == R.id.home) {
+                replaceFragment(new HomeFragment());
+            } else if (id == R.id.profile) {
+                replaceFragment(new ProfileFragment());
+            } else if (id == R.id.saved) {
+                replaceFragment(new SavedFragment());
+            }
+            return true;
+        });
+
+    }
+    private void replaceFragment(Fragment fragment){
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.frame_layout,fragment);
+        fragmentTransaction.commit();
+
+
+    }
 
 }
