e803a063e48503e5e8779c453f1a1410569f39c2
nazar001 <nazar-m1@ulster.ac.uk>
2025-12-06 15:28:50 +0000

    changed splashscreen to chek if user is logged in or not

--- app/src/main/java/com/example/courseworkproject/SplashActivity.java
+++ app/src/main/java/com/example/courseworkproject/SplashActivity.java
@@ -7,15 +7,24 @@ import android.os.Looper;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.firebase.auth.FirebaseAuth;
+
 public class SplashActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.splash_screen);
 
-        // Delay 2 seconds then open MainActivity
+        FirebaseAuth auth = FirebaseAuth.getInstance();
+
         new Handler(Looper.getMainLooper()).postDelayed(() -> {
-            startActivity(new Intent(SplashActivity.this, LoginActivity.class));
+            if (auth.getCurrentUser() != null) {
+                // User is logged in, go to Home
+                startActivity(new Intent(SplashActivity.this, HomeActivity.class));
+            } else {
+                // User not logged in, go to Login
+                startActivity(new Intent(SplashActivity.this, LoginActivity.class));
+            }
             finish();
         }, 2000);
     }
